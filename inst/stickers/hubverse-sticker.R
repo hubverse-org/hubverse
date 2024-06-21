81b77abeb03cbf0104faa21dae76399eaa3e5ae2
Nicholas Reich <nick@umass.edu>
2024-06-21 12:39:02 -0400

    renaming icon file to remove space and adding spaces around infix operators

--- inst/stickers/hubverse-sticker.R
+++ inst/stickers/hubverse-sticker.R
@@ -1,7 +1,7 @@
 library(hexSticker)
 library(ggplot2)
 
-imgurl <- "./inst/stickers/Icon 1000-1000px.png"
+imgurl <- "./inst/stickers/Icon-1000-1000px.png"
 
 library(showtext)
 ## Loading Google fonts (http://www.google.com/fonts)
@@ -12,9 +12,9 @@ showtext_auto()
 
 sticker(imgurl, package="hubverse",
         ## font info
-        p_size=18, p_y=1.4, p_color = "#3C88BE", p_family = "poppins",
+        p_size = 18, p_y = 1.4, p_color = "#3C88BE", p_family = "poppins",
         ## image info
-        s_width=.7, s_x = 1,
+        s_width = 0.7, s_x = 1,
         ## background info
         h_fill = "white", h_color = "#3C88BE", h_size = 1.5,
-        filename="./inst/stickers/hubverse-sticker.png")
+        filename = "./inst/stickers/hubverse-sticker.png")
